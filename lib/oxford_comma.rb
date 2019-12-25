048832e0cf2e4701c481054317b73febfd8efe62
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-25 06:36:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,15 +2,7 @@ def oxford_comma(array)
   if array.length < 3
     array.join(" and ")
   else 
-    # create a new array containing the last two elements
-    last = array.slice(array.length - 2, array.length - 1)
-    last_string = last.join(" and ")
-    if array.length == 3
-      rest_string = array[0].to_s + ", " 
-    else 
-      rest = array.slice(0, array.length - 3)
-      rest_string = rest.join(", ")
-    end
-    return final_string = rest_string + last_string
-  end
+    string = array.join(", ")
+    string[string.length - 1] = "and " + string[string.length - 1]
+    return string
 end
