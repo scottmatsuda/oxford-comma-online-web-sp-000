0810a05d456af663ead3eedc8d68fb2c0cd6d1bc
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-25 06:39:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,6 @@ def oxford_comma(array)
     array.join(" and ")
   else 
     string = array.join(", ")
-    string[string.last.length - 1] = "and " + string[string.last.length - 1]
     return string
   end
     
