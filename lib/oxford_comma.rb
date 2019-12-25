737194219065fe74e3793811df6c60146128ec8b
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-25 06:10:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,15 @@
 def oxford_comma(array)
   if array.length < 3
     array.join(" and ")
+  else 
+    # create a new array containing the last two elements
+    last = []
+    last << array[array.length - 2]
+    last << array[array.length - 1]
+    last.join(" and ")
+    
+    # create a new array containing the rest of the elements
+    # join the array with a ", "
+    # concatenate and return the two strings
   end
 end
