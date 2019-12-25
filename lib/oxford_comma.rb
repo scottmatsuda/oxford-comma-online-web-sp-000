148a18ecd586429c7a7564025a007b87bd688159
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-25 06:15:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,14 +3,11 @@ def oxford_comma(array)
     array.join(" and ")
   else 
     # create a new array containing the last two elements
-    last = []
-    last << array[array.length - 2]
-    last << array[array.length - 1]
+    last = array.slice(array.length - 2, array.length - 1)
     last.join(" and ")
     rest = array.slice(0, array.length - 3)
-    
-    # create a new array containing the rest of the elements
-    # join the array with a ", "
-    # concatenate and return the two strings
+    rest.join(", ")
+    final = rest + last
+    return final
   end
 end
