fe6f5ffb4e316d97ccd4ca6b97ff0ca685fb6cb9
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-25 06:40:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   if array.length < 3
     array.join(" and ")
   else 
+    array[array.length - 1] = "and " + array[array.length - 1]
     string = array.join(", ")
     return string
   end
